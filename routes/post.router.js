9e1a5fd32b586ef7e07fe50b66c924b5baeae0ec
Juan J. Lagunas <lagunasjuan_isc09@hotmail.com>
2024-01-19 08:30:13 -0600

    delete and select comment

--- routes/post.router.js
+++ routes/post.router.js
@@ -5,7 +5,7 @@ const service = new PostService();
 
 router.get('/', async (req, res, next) => {
   try {
-    const response = await service.findAllPost();
+    const response = await service.findAllPost(); // ENLISTA TODOS LOS POST
     res.status(200).json({ data: response });
   } catch (e) {
     next(e);
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
 
 router.post('/newpost', async (req, res, next) => {
   try {
-    const response = await service.createPost({ ...req.body });
+    const response = await service.createPost({ ...req.body }); // AÑADE UN NUEVO POST
     res.status(200).json({ data: response });
   } catch (e) {
     next(e);
@@ -23,27 +23,36 @@ router.post('/newpost', async (req, res, next) => {
 
 router.post('/likepost', async (req, res, next) => {
   try {
-    const response = await service.likePostByUser({ ...req.body });
+    const response = await service.likePostByUser({ ...req.body }); //AÑADE EL LIKE DEL USUARIO
     res.status(200).json({ data: response });
   } catch (e) {
     next(e);
   }
 });
 
-router.delete('/deletepost/:id', async (req, res, next) => {
-  const { id } = req.params;
+router.delete('/likepost/:idpost/:iduser', async (req, res, next) => {
+  const { idpost,  iduser} = req.params;
   try {
-    const response = await service.deletePost(id);
+    const response = await service.deleteLikePostByUser(idpost,iduser); // ELIMINA EL LIKE DEL USUARIO
     res.status(200).json({ data: response });
   } catch (e) {
     next(e);
   }
 });
 
-router.get('/:id', async (req, res, next) => {
-  const { id } = req.params;
+router.post('/commets', async (req, res, next) => {
   try {
-    const response = await service.findPost(id);
+    const response = await service.commentsByPost({ ...req.body});
+    res.status(200).json({ data: response }); // ENLISTA LOS COMENTARIOS
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete('/comments/:idpost/:idcomment', async (req, res, next) => {
+  const { idpost,  idcomment} = req.params;
+  try {
+    const response = await service.deleteCommentByUSer(idpost,idcomment); // ELIMINA LOS COMENTARIOS POR ID DEL COMENTARIO
     res.status(200).json({ data: response });
   } catch (e) {
     next(e);
@@ -53,7 +62,27 @@ router.get('/:id', async (req, res, next) => {
 router.get('/byuser/:idUser', async (req, res, next) => {
   const { idUser } = req.params;
   try {
-    const response = await service.findPostByUser(idUser);
+    const response = await service.findPostByUser(idUser); // BUSCA POSTS POR USUARIO
+    res.status(200).json({ data: response });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const response = await service.findPost(id); // BUSCA POST POR ID
+    res.status(200).json({ data: response });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete('/deletepost/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const response = await service.deletePost(id); // ELIMINA EL POST
     res.status(200).json({ data: response });
   } catch (e) {
     next(e);
@@ -61,6 +90,14 @@ router.get('/byuser/:idUser', async (req, res, next) => {
 });
 
 
-
+router.patch('/updatecollection/:idUser', async (req, res, next) => {
+  const { idUser } = req.params;
+  try {
+    const response = await service.updateCollection(idUser, { ...req.body }); // ACTUALIZA LA COLLECCION CUANDO EL USUARIO SE MODIFICA
+    res.status(200).json({ data: response });
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
--- services/post.service.js
+++ services/post.service.js
@@ -4,41 +4,66 @@ const postModel = mongoose.model('posts', postSchema);
 
 
 class PostService {
-    async findAllPost() {
-        const result = await postModel.find({ "user.status": 1 }).exec();
-        return await result;
-    }
+  async findAllPost() {
+    const result = await postModel.find({ "user.status": 1 }).exec();
+    return await result;
+  }
 
-    async findPost(idPost) {
-        const result = await postModel.find({ "id": idPost, "user.status": 1 }).exec();
-        /*  const comentarios = querySelectOne.comments; // Obtiene el arreglo de objetos de comentarios por publicacion
-         console.log(Object.keys(comentarios).length); // Obtiene el numero total de comentarios*/
-        return await result;
-    }
+  async findPost(idPost) {
+    const result = await postModel.find({ "id": idPost, "user.status": 1 }).exec();
+    /*  const comentarios = querySelectOne.comments; // Obtiene el arreglo de objetos de comentarios por publicacion
+     console.log(Object.keys(comentarios).length); // Obtiene el numero total de comentarios*/
+    return await result;
+  }
 
-    async createPost(dataPost) {
-        const result = new postModel({...dataPost});
-        await result.save();
-        return result;
-    }
+  async createPost(dataPost) {
+    const result = new postModel({ ...dataPost });
+    await result.save();
+    return result;
+  }
 
-    async deletePost(idPost){
-      const result = await postModel.deleteOne({ "_id": idPost });
-      console.log(result)
-      return idPost;
-    }
+  async deletePost(idPost) {
+    const result = await postModel.deleteOne({ "_id": idPost });
+    console.log(result)
+    return idPost;
+  }
 
-    async findPostByUser(userId) {
-      const result = await postModel.find({ "user.id" : userId }).exec();
-      return await result;
+  async findPostByUser(userId) {
+    const result = await postModel.find({ "user.id": userId }).exec();
+    return await result;
   }
 
   async likePostByUser(dataPost) {
-    console.log(dataPost.idPost)
-    const result = await postModel.updateOne({ "_id": dataPost.idPost }, { 'likes': dataPost.likes });
+    const result = await postModel.updateMany({ "_id": dataPost.idPost }, { $push: { 'likes': dataPost.likes } });
     return result;
-}
+  }
 
+  async deleteLikePostByUser(idpost, iduser) {
+    const result = await postModel.updateMany({ "_id": idpost }, { $pull: { "likes": { "userId": iduser } } });
+    return result;
+  }
+
+  //LOS COMENTARIOS POR USUARIO EN EL POST SE OBTENDRAN DESDE EL FROND
+  //ESTE METODO REGISTRA NUEVOS COMENTARIOS
+  async commentsByPost(dataPost) {
+    const result = await postModel.updateMany({ "_id": dataPost.idPost }, { $push: { 'comments': dataPost.comments } });
+    return result;
+  }
+
+  async deleteCommentByUSer(idpost, idcomment) {
+    const result = await postModel.updateMany({ "_id": idpost }, { $pull: { "comments": { "_id": idcomment } } });
+    return result;
+  }
+
+  async updateCollection(idUser, dataPost) {
+    console.log(idUser)
+    const result = await postModel.updateMany({ "user.id": idUser},
+      {$set: {
+        "coments.$.nickname":  dataPost.user.nickname
+      }});
+
+      return result;
+  }
 
 }
 
