b4847fb7ac1cdf14c84f2285f247eb21289cd4d5
Bryan Saladino <bryan@git.com>
2024-01-31 17:38:25 -0600

    nothing

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -19,41 +19,51 @@ router.post('/signin', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const result = await service.store({ ...req.body });
-    return res.status(201).json({ result });
+    return res.status(201).json({ data: result });
   } catch (e) {
     next(e);
   }
 });
 
+router.get(
+  '/user',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const result = await service.selectUser(req.user.sub);
+      return res.status(201).json({ result });
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
+router.patch(
+  '/user/update',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const result = await service.updateUser(req.user.sub, { ...req.body });
+      return res.status(201).json({ result });
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
-router.get('/user', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-  try {
-    const result = await service.selectUser(req.user.sub);
-    return res.status(201).json({ result });
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.patch('/user/update', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-  try {
-    const result = await service.updateUser(req.user.sub, { ...req.body });
-    return res.status(201).json({ result });
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.patch('/user/password', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-  try {
-    const result = await service.updateCurrentPass(req.user.sub, { ...req.body });
-    return res.status(201).json({ result });
-  } catch (e) {
-    next(e);
-  }
-});
-
-
+router.patch(
+  '/user/password',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const result = await service.updateCurrentPass(req.user.sub, {
+        ...req.body,
+      });
+      return res.status(201).json({ result });
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
 module.exports = router;
--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  nickname: { type: String, unique: true },
-  name: String,
-  lastname: String,
-  motherlastname: String,
-  email: { type: String, unique: true },
-  password: String,
-  phonecode: {
-    ode: String,
-    dial_code: String,
-    flag: String,
-    name: String
-},
-  phone: { type: String, unique: true },
-  country: String,
-  state: String,
-  city: String,
-  sex: String,
-  age: Number,
-  image: String,
-  rol: String,
-  status: Number
-},  { timestamps: true });
+const userSchema = mongoose.Schema(
+  {
+    nickname: { type: String, unique: true },
+    name: String,
+    lastname: String,
+    motherlastname: String,
+    email: { type: String, unique: true },
+    password: String,
+    phonecode: {
+      ode: String,
+      dial_code: String,
+      flag: String,
+      name: String,
+    },
+    phone: { type: String, unique: true },
+    country: String,
+    state: String,
+    city: String,
+    sex: String,
+    age: Number,
+    image: String,
+    rol: String,
+    status: Number,
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('user', userSchema);
