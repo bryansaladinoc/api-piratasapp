e5c2c1a3e194f4dfcb4835a65f3349e61f0d3eff
Bryan Saladino <bryan@git.com>
2024-02-01 17:40:49 -0600

    feat

--- services/auth.service.js
+++ services/auth.service.js
@@ -40,31 +40,29 @@ class AuthService {
     await session.startTransaction();
     try {
       const user = await User.findOne({
-        "_id": idUser, "password": data.currentPass
+        _id: idUser,
+        password: data.currentPass,
       }).exec();
 
       if (user) {
-        await User.updateOne({ "_id": idUser, }, { "password": data.newPass });
+        await User.updateOne({ _id: idUser }, { password: data.newPass });
         await session.commitTransaction();
-        return true
+        return true;
       }
 
       await session.commitTransaction();
-      return false
-
+      return false;
     } catch (err) {
-
       await session.abortTransaction();
-      console.log(err)
-      return false
-
+      console.log(err);
+      return false;
     } finally {
       await session.endSession();
     }
   }
 
   async selectUser(idUser) {
-    const user = await User.findOne({ "_id": idUser }).exec();
+    const user = await User.findOne({ _id: idUser }).exec();
     return user;
   }
 
@@ -82,63 +80,72 @@ class AuthService {
 
     try {
       //ACTUALIZAR USUARIO
-      await User.updateOne({ "_id": idUser }, 
-      { 
-        "name": data.name,
-        "lastname": data.lastname,
-        "motherlastname": data.motherlastname,
-        "email": data.email,
-        "phonecode": data.phonecode,
-        "phone": data.phone,
-        "country": data.country,
-        "state": data.state,
-        "city": data.city,
-        "sex": data.sex,
-        "age": data.age,
-        "image": data.image,
-        "rol": data.rol,
-        "status": data.status,
-      }, { session });
+      await User.updateOne(
+        { _id: idUser },
+        {
+          name: data.name,
+          lastname: data.lastname,
+          motherlastname: data.motherlastname,
+          email: data.email,
+          phonecode: data.phonecode,
+          phone: data.phone,
+          country: data.country,
+          state: data.state,
+          city: data.city,
+          sex: data.sex,
+          age: data.age,
+          image: data.image,
+          rol: data.rol,
+          status: data.status,
+        },
+        { session },
+      );
 
       // ACTUALIZA LA INFORMACION DEL USARIO EN TODOS LOS POST QUE EL HAYA REALIZADO
-      const filterPost = { "user.idUser": idUser }; 
-      const updatePost = await postModel.updateMany(filterPost,
+      const filterPost = { 'user.idUser': idUser };
+      const updatePost = await postModel.updateMany(
+        filterPost,
         {
-          "user.name": data.name,
-          "user.lastname": data.lastname,
-          "user.motherlastname": data.motherlastname,
-          "user.imageUserUri": data.image,
-          "user.rol": data.rol,
-          "user.status": data.status,
-        }, { session })
+          'user.name': data.name,
+          'user.lastname': data.lastname,
+          'user.motherlastname': data.motherlastname,
+          'user.imageUserUri': data.image,
+          'user.rol': data.rol,
+          'user.status': data.status,
+        },
+        { session },
+      );
 
       // ACTUALIZA TODOS LOS  COMENTARIOS QUE EL USUARIO HAYA REALIZADO EN TODOS LOS POSTS
-      const filterComment = { "comments.idUser": idUser }; // CONDICION PARA EL QUERY
+      const filterComment = { 'comments.idUser': idUser }; // CONDICION PARA EL QUERY
       const actualizacion = {
         $set: {
-          "comments.$[element].name": data.name,
-          "comments.$[element].lastname": data.lastname,
-          "comments.$[element].motherlastname": data.motherlastname,
-          "comments.$[element].imageUserUri": data.image,
-          "comments.$[element].rol": data.rol,
-          "comments.$[element].userStatus": data.status
-        }
+          'comments.$[element].name': data.name,
+          'comments.$[element].lastname': data.lastname,
+          'comments.$[element].motherlastname': data.motherlastname,
+          'comments.$[element].imageUserUri': data.image,
+          'comments.$[element].rol': data.rol,
+          'comments.$[element].userStatus': data.status,
+        },
       };
 
       const opciones = {
         session: session,
-        arrayFilters: [{ "element.idUser": idUser }], // CONDICION PARA EL ARREGLO
-        multi: true // IMPORTANTE
+        arrayFilters: [{ 'element.idUser': idUser }], // CONDICION PARA EL ARREGLO
+        multi: true, // IMPORTANTE
       };
-      const queryD = await postModel.updateMany(filterComment, actualizacion, opciones);
-
+      const queryD = await postModel.updateMany(
+        filterComment,
+        actualizacion,
+        opciones,
+      );
 
       await session.commitTransaction();
-      return true
+      return true;
     } catch (err) {
       await session.abortTransaction();
-      console.log(err)
-      return fasle
+      console.log(err);
+      return fasle;
     } finally {
       await session.endSession();
     }
